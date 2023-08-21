4c9347e15edbb1715da8395e7bc90a7c4075e89c
Pekka Enberg <penberg@iki.fi>
2023-08-21 11:12:54 +0300

    Add libsql.sqlite_version_info property
    Needed by SQLAlchemy, for example.

--- src/lib.rs
+++ src/lib.rs
@@ -148,6 +148,7 @@ fn convert_row(py: Python, row: libsql_core::rows::Row, column_count: i32) -> Py
 #[pymodule]
 fn libsql_experimental(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("paramstyle", "qmark")?;
+    m.add("sqlite_version_info", (3, 42, 0))?;
     m.add_function(wrap_pyfunction!(connect, m)?)?;
     m.add_class::<Connection>()?;
     m.add_class::<Cursor>()?;
