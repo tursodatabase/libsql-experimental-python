1705759f874480080092f45bade66b8125918db7
Martin Olivesten <marol450@student.liu.se>
2024-03-16 01:33:11 +0100

    Some changes i thought of while trying to sleep

--- libsql_experimental.pyi
+++ libsql_experimental.pyi
@@ -1,34 +1,29 @@
 """Type stubs for libSQL's rust extensions"""
 
-from typing import Any, Sequence, Optional, Literal
-from typing_extensions import Self
-
-
-IsolationLevel = Literal['DEFERRED', 'IMMEDIATE', 'EXCLUSIVE']
+from typing import Any
 
 
 class Cursor:
     """libSQL database cursor.
 
     Implements a superset of the [DB-API 2.0 (PEP249)](https://peps.python.org/pep-0249/) Cursor object protocol."""  # noqa: E501
+    arraysize: int
+
     @property
-    def description(self) -> Optional[Sequence[Sequence[Any]]]: ...
+    def description(self) -> tuple[tuple[Any, ...], ...] | None: ...
 
     @property
     def rowcount(self) -> int: ...
 
     @property
-    def arraysize(self) -> int: ...
-
-    @property
-    def lastrowid(self) -> Optional[int]: ...
+    def lastrowid(self) -> int | None: ...
 
     def close(self) -> None: ...
-    def execute(self, sql: str, parameters: Sequence[Any] = ...) -> Self: ...
-    def executemany(self, sql: str, parameters: Sequence[Sequence[Any]] = ...) -> Self: ...  # noqa: E501
-    def fetchone(self) -> Optional[Sequence[Any]]: ...
-    def fetchmany(self, size: int = ...) -> Optional[Sequence[Sequence[Any]]]: ...  # noqa: E501
-    def fetchall(self) -> Optional[Sequence[Sequence[Any]]]: ...
+    def execute(self, sql: str, parameters: tuple[Any] = ...) -> 'Cursor': ...
+    def executemany(self, sql: str, parameters: list[tuple[Any]] = ...) -> 'Cursor': ...  # noqa: E501
+    def fetchone(self) -> tuple[Any] | None: ...
+    def fetchmany(self, size: int = ...) -> list[tuple[Any, ...]]: ...  # noqa: E501
+    def fetchall(self) -> list[tuple[Any]]: ...
 
 
 class Connection:
@@ -36,10 +31,7 @@ class Connection:
 
     Implements a superset of the [DB-API 2.0 (PEP249)](https://peps.python.org/pep-0249/) Connection object protocol."""  # noqa: E501
     @property
-    def autocommit(self) -> bool: ...
-
-    @property
-    def isolation_level(self) -> Optional[IsolationLevel]: ...
+    def isolation_level(self) -> str | None: ...
 
     @property
     def in_transaction(self) -> bool: ...
@@ -48,15 +40,15 @@ class Connection:
     def cursor(self) -> Cursor: ...
     def sync(self) -> None: ...
     def rollback(self) -> None: ...
-    def execute(self, sql: str, parameters: Sequence[Any] = ...) -> Cursor: ...
-    def executemany(self, sql: str, parameters: Sequence[Sequence[Any]] = ...) -> Cursor: ...  # noqa: E501
+    def execute(self, sql: str, parameters: tuple[Any] = ...) -> Cursor: ...
+    def executemany(self, sql: str, parameters: list[tuple[Any]] = ...) -> Cursor: ...  # noqa: E501
     def executescript(self, script: str) -> None: ...
 
 
 def connect(database: str,
-            isolation_level: Optional[IsolationLevel] = ...,
-            check_same_thread: bool = ...,
-            uri: bool = ...,
+            isolation_level: str | None = 'DEFERRED',
+            check_same_thread: bool = True,
+            uri: bool = False,
             sync_url: str = ...,
             sync_interval: float = ...,
             auth_token: str = ...,
