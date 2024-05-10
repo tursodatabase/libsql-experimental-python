2bdd16ace58a03665fca2ae244cb92d73ab70021
Shubham Pawar <shubham@sveltespot.com>
2024-05-10 12:07:57 +0530

    Fix #54

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -159,7 +159,21 @@ def test_in_transaction(provider):
     cur.execute("INSERT INTO users VALUES (?, ?)", (2, 'bob@example.com'))
     assert conn.in_transaction == True
 
+@pytest.mark.parametrize("provider", ["libsql-remote", "libsql", "sqlite"])
+def test_fetch_expression(provider):
+    dbname = "/tmp/test.db" if provider == "libsql-remote" else ":memory:"
+    conn = connect(provider, dbname)
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS users")
+    cur.execute("CREATE TABLE users (id INTEGER, email TEXT)")
+    cur.execute("INSERT INTO users VALUES (1, 'alice@example.com')")
+    res = cur.execute("SELECT QUOTE(email) FROM users")
+    assert [("'alice@example.com'",)] == res.fetchall()
+
+
 def connect(provider, database, isolation_level='DEFERRED'):
+    if provider == "libsql-remote":
+        return libsql_experimental.connect(database, sync_url="http://localhost:8080", auth_token="")
     if provider == "libsql":
         return libsql_experimental.connect(database, isolation_level = isolation_level)
     if provider == "sqlite":
