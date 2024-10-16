d031c9b1b59f9175f6012fd94b7586fb7f57ee04
Giovanni <giovanni.benussi@usach.cl>
2024-10-16 09:40:45 -0300

    Add vector example

---
+++ examples/vector/README.md
@@ -0,0 +1,19 @@
+# Local
+
+This example demonstrates how to use libSQL vector search with a local database.
+
+## Install Dependencies
+
+```bash
+pip install libsql-experimental
+```
+
+## Running
+
+Execute the example:
+
+```bash
+python3 main.py
+```
+
+This will setup a local SQLite database, insert some data, and then query the results using the vector similarity search function.
---
+++ examples/vector/main.py
@@ -0,0 +1,10 @@
+import libsql_experimental as libsql
+
+conn = libsql.connect("local.db")
+
+conn.execute("DROP TABLE IF EXISTS movies")
+conn.execute("CREATE TABLE IF NOT EXISTS movies (title TEXT, year INT, embedding F32_BLOB(3))")
+conn.execute("CREATE INDEX movies_idx ON movies (libsql_vector_idx(embedding))")
+conn.execute("INSERT INTO movies (title, year, embedding) VALUES ('Napoleon', 2023, vector32('[1,2,3]')), ('Black Hawk Down', 2001, vector32('[10,11,12]')), ('Gladiator', 2000, vector32('[7,8,9]')), ('Blade Runner', 1982, vector32('[4,5,6]'))")
+
+print(conn.execute("SELECT title, year FROM vector_top_k('movies_idx', '[4,5,6]', 3) JOIN movies ON movies.rowid = id").fetchall())
